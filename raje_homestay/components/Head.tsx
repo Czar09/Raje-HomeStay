62911cfe0319af157bd37c46c178721ab55cc7f0
karan kartikeya <karankartikey72@gmail.com>
2022-11-13 12:32:07 +0530

    added globals.css with tailwind file

---
+++ raje_homestay/components/Head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Head({}: Props) {
+  return (
+    <div>
+        <h1 className='text-red-200 text-4xl'>hello header</h1>
+    </div>
+  )
+}
+
+export default Head
