0c82dc2f49709032146f3c1e954e72dac763f0c7
karan kartikeya <karankartikey72@gmail.com>
2022-11-15 00:25:40 +0530

    added banner

---
+++ raje_homestay/components/Banner.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {}
+
+function Banner({}: Props) {
+  return (
+    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+        <Image
+        priority
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        alt ="banner"
+        objectFit="cover"
+        />
+        <div className='absolute top-1/2 w-full text-center'>
+            <p>Not sure what to choose? Perfect</p>
+            <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'> 
+                I'm Flexible
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- raje_homestay/components/Head.tsx
+++ raje_homestay/components/Head.tsx
@@ -25,14 +25,14 @@ function Head({}: Props) {
 
       {/**  Middle Section: Search Bar */}
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-md'>
-        <input type='text'  className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400' placeholder='Start your Search here'/>
+        <input type='text'  className='flex-grow pl-3 bg-transparent outline-none text-gray-600 placeholder-gray-400' placeholder='Search here'/>
         <SearchIcon className='hidden md:inline-flex  text-white h-8 bg-red-300 rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
 
       {/**  Right Section: Right Side */}
       <div className='flex items-center space-x-4 justify-end text-gray-500'>
        <p className='hidden lg:inline cursor-pointer'> Book Gypsy Now!!</p>
-       <GlobeAltIcon className='h-6'/>
+       <GlobeAltIcon className='hidden sm:inline h-6'/>
       
       <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
         <MenuIcon className='h-6'/>
