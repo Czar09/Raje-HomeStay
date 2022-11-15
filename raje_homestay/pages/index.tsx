d32c41a598418bd04f5238787947f9ddfaa9ba66
karan kartikeya <karankartikey72@gmail.com>
2022-11-15 23:23:27 +0530

    added cards

--- raje_homestay/pages/index.tsx
+++ raje_homestay/pages/index.tsx
@@ -1,12 +1,26 @@
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Head from '../components/Head'
+import MediumCard from '../components/MediumCard'
 
 export default function Home() {
   return (
     <div>
-      <Head/>
-      <Banner/>
+      <Head />
+      <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl py-8 font-semibold'>
+            Explore These
+          </h2>
+        </section>
+        <div className='flex overflow-scroll space-x-6 scrollbar-hide'>
+          <MediumCard />
+        </div>
+      </main>
+
     </div>
   )
 }
+
+
