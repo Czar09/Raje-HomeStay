93d12f07ee95529122fc60cbc241a9f5ced1db03
karan kartikeya <karankartikey72@gmail.com>
2022-12-02 20:57:09 +0530

    feat: added URL

--- raje_homestay/utils/fetchAmenities.ts
+++ raje_homestay/utils/fetchAmenities.ts
@@ -1,7 +1,7 @@
 import { Amenities } from "../typing";
 
 export const fetchAmenities = async() =>{
-    const url = new URL('/api/getAmenities',`${process.env.NEXT_PUBLIC_BASE_URL}`)
+    const url = new URL("/api/getAmenities",`${process.env.NEXT_PUBLIC_BASE_URL}`)
     const res = await fetch(url);
     const data = await res.json();
     const amenities: Amenities[] = data.amenities;
